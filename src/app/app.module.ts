e9cab242c5042a224948c559a804603cc69bb679
emilianog4 <emiliano.gutierrez4@gmail.com>
2022-04-21 17:35:32 -0300

    Componentes
    En esta parte se incluyen todos los componentes que va a contener el portfolio.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { AptitudesComponent } from './components/aptitudes/aptitudes.component';
+import { ExperienciaYEducacionComponent } from './components/experiencia-y-educacion/experiencia-y-educacion.component';
+import { LogrosComponent } from './components/logros/logros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    AptitudesComponent,
+    ExperienciaYEducacionComponent,
+    LogrosComponent
   ],
   imports: [
     BrowserModule,
